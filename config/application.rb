743f2be9e4a47d62c5a61623434090cd36311115
MrFuku <mmafighter_shun@icloud.com>
2021-08-31 22:23:21 +0900

    Modify generate files

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,10 @@ module Rails526
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.assets false
+      g.skip_routes false
+      g.test_framework :rspec
+    end
   end
 end
