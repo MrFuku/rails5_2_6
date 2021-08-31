bd6ebabd768767a3d39790c1e2d03242aed23529
MrFuku <mmafighter_shun@icloud.com>
2021-08-31 22:06:29 +0900

    Add setup script

---
+++ scripts/setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# apt（Macで言うところのbrew）の更新
+sudo apt update -y
+
+# 環境構築に必要なパッケージのインストール
+sudo apt install -y build-essential libssl-dev libreadline-dev zlib1g-dev libsqlite3-dev
+
+# rbenvのセットアップ
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+echo 'export PATH=$HOME/.rbenv/bin:$PATH' >> ~/.bashrc
+export PATH=$HOME/.rbenv/bin:$PATH
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+eval "$(rbenv init -)"
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+# rubyのインストール
+rbenv install 2.7.2
+rbenv global 2.7.2
+
+# nodeのセットアップ
+git clone https://github.com/nodenv/nodenv.git ~/.nodenv
+echo 'export PATH=$HOME/.nodenv/bin:$PATH' >> ~/.bashrc
+export PATH=$HOME/.nodenv/bin:$PATH
+echo 'eval "$(nodenv init -)"' >> ~/.bashrc
+eval "$(nodenv init -)"
+git clone https://github.com/nodenv/node-build.git $(nodenv root)/plugins/node-build
+
+# nodeのインストール
+nodenv install 14.17.5
+nodenv global 14.17.5
+
+# railsのセットアップ
+bundle
+rbenv rehash
